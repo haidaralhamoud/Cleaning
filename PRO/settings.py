9ab7b45f57fe5a8f38ee4625786821f9231864f8
unknown <haidaralhamoud@gmail.com>
2025-11-16 12:45:54 +0300

    2 Edit

--- PRO/settings.py
+++ PRO/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
+CORS_ALLOW_ALL_ORIGINS = True
---
+++ home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+from . import views
+app_name='home'
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('about', views.about,name='about'),
+]
