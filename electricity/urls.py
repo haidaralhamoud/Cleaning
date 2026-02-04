027de9736aa15e8f4631a738bdf550a64f66bf75
layal <layal.hasn11@gmail.com>
2026-02-04 13:08:21 +0300

    lh

--- electricity/urls.py
+++
@@ -1,9 +0,0 @@
-﻿from django.urls import path
-from . import views
-
-app_name = "electricity"
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("services/", views.services, name="services"),
-]
