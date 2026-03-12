2891d886df7d6ce405202cf7d41d5fb238682b75
layal <layal.hasn11@gmail.com>
2026-03-12 17:56:52 +0300

    pay2

--- PRO/settings.py
+++ PRO/settings.py
@@ -169,16 +169,16 @@ WSGI_APPLICATION = 'PRO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'cleaning'),
-#         'USER': os.getenv('POSTGRES_USER', 'cleaning'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-#         'PORT': os.getenv('POSTGRES_PORT', '5432'),
-#     },
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'cleaning'),
+        'USER': os.getenv('POSTGRES_USER', 'cleaning'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    },
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
